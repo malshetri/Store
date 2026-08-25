e5450bb10cdd5a2853224eb78369f1e8bd6b6890
Muneer Al-Shetri <muneer.alshetri@gmail.com>
2026-08-25 18:05:22 +0200

    Add cart items

--- src/main/java/com/muneer/store/dtos/CartItemDto.java
+++ src/main/java/com/muneer/store/dtos/CartItemDto.java
@@ -1,4 +1,13 @@
 package com.muneer.store.dtos;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class CartItemDto {
+    private CartProductDto product;
+    private Integer quantity;
+    private BigDecimal totalPrice;
+
 }
--- src/main/java/com/muneer/store/mappers/CartMapper.java
+++ src/main/java/com/muneer/store/mappers/CartMapper.java
@@ -1,10 +1,14 @@
 package com.muneer.store.mappers;
 
 import com.muneer.store.dtos.CartDto;
+import com.muneer.store.dtos.CartItemDto;
 import com.muneer.store.entities.Cart;
+import com.muneer.store.entities.CartItem;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CartMapper {
     CartDto toDto(Cart cart);
+    CartItemDto toDto(CartItem cartItem);
 }

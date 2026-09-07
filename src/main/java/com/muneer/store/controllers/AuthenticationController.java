e5ad350aac8842024c05ee1b77c83d8d552c8226
Muneer Al-Shetri <muneer.alshetri@gmail.com>
2026-09-07 11:56:24 +0200

    Add JWT generation

--- src/main/java/com/muneer/store/controllers/AuthenticationController.java
+++ src/main/java/com/muneer/store/controllers/AuthenticationController.java
@@ -1,15 +1,19 @@
 package com.muneer.store.controllers;
 
+import com.muneer.store.dtos.JwtResponse;
 import com.muneer.store.dtos.LoginRequest;
 import com.muneer.store.repositories.UserRepository;
+import com.muneer.store.services.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
 
 @RestController
 @AllArgsConstructor
@@ -17,27 +21,24 @@ import java.util.Map;
 
 public class AuthenticationController {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(
+    public ResponseEntity<JwtResponse> login(
             @Valid @RequestBody LoginRequest request
             ) {
-        var user = userRepository.findByEmail(request.getEmail()).orElse(null);
-        if (user == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                request.getEmail(), request.getPassword()
+        ));
+        var token = jwtService.generateToken(request.getEmail());
 
-        if (!passwordEncoder.matches(
-                request.getPassword(),
-                user.getPassword()
-        )) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body(Map.of("error", "The email or password is incorrect"));
-        }
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Void> handleBadCredentialsExeption(){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
     }
 }
